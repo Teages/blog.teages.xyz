0f4da77c2f96ce6943b11d899e0018ee2a8a84c1
Teages <Teages@teages.xyz>
2021-06-12 22:04:36 +0800

    迁移, 基础功能

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# blog.teages.xyz
-A blog powered by VuePress.
+# Teages's Blog
+
+文章均遵循 CC BY-NC-SA 4.0 共享, 保留部分权利. (有另行说明的除外)
+
+依赖项:
+
+- Jekll
+  - jekyll-feed
+  - jekyll-seo-tag
+- MDUI
---
+++ assets/js/toc.js
@@ -0,0 +1,13 @@
+// window.onload = function() {
+//   console.log("###")
+//   let $ = mdui.$;
+//   let haeder_ps = $('main #article-markdown h1, h2, h3, h4, h5, h6');
+//   for (let header_p in haeder_ps) {
+//     const header = haeder_ps[header_p];
+//     console.log(header)
+//     const headerLevel = header.tagName.toLowerCase();
+//     const $header = $(header);
+//     const headerName = $header.text().trim();
+//     console.log(headerLevel, headerName);
+//   }
+// }
